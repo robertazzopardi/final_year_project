fe657aedacfb802f138059fc8e3c2dd9ea516927
robertazzopardi <robertazzopardi@icloud.com>
2020-12-31 18:36:40 +0000

    added the 4 hunter robots to the environment

--- src/java/myEnv.java
+++ src/java/myEnv.java
@@ -15,7 +15,8 @@ public class myEnv extends Environment {
 	private Logger logger = Logger.getLogger("final_year_project." + myEnv.class.getName());
 
 	EnvController controller;
-	RobotMonitor myRobot;
+//	RobotMonitor myRobot;
+	RobotMonitor[] robotMonitors;
 
 	/** Called before the MAS execution with the args informed in .mas2j */
 	@Override
@@ -27,20 +28,36 @@ public class myEnv extends Environment {
 			e.printStackTrace();
 		}
 
-		controller = new EnvController("/Users/rob/_CODE/Java/final-project/defaultCongif.txt", 7, 6);
-		myRobot = controller.getMyRobot();
+		controller = new EnvController("/Users/rob/_CODE/Java/final-project/defaultConfig.txt", 10, 10);
+//		myRobot = controller.getMyRobot();
+		robotMonitors = controller.getRobots();
 
-		myRobot.monitorRobotStatus(true);
-		myRobot.setTravelSpeed(100); // 10cm per sec myRobot.setDirection(0);
-		while (myRobot.getUSenseRange() > 700) {
-			myRobot.travel(350); // travel 35 cm
+//		robotMonitors[0].monitorRobotStatus(true);
+//		robotMonitors[0].setTravelSpeed(100); // 10cm per sec myRobot.setDirection(0);
+//		while (robotMonitors[0].getUSenseRange() > 700) {
+//			robotMonitors[0].travel(350); // travel 35 cm
+//		}
+//		robotMonitors[0].rotate(90); // Turn Left
+//		while (!robotMonitors[0].isBumperPressed()) {
+//			robotMonitors[0].travel(350); // travel 35 cm
+//			if (robotMonitors[0].getCSenseColor().equals(Color.GREEN)) {
+//				System.out.println("found Green cell at location (" + robotMonitors[0].getX() + ","
+//						+ robotMonitors[0].getY() + ") with heading " + robotMonitors[0].getHeading());
+//			}
+//		}
+
+		robotMonitors[1].monitorRobotStatus(true);
+		robotMonitors[1].setTravelSpeed(100); // 10cm per sec myRobot.setDirection(0);
+		robotMonitors[1].rotate(90);
+		while (robotMonitors[1].getUSenseRange() > 700) {
+			robotMonitors[1].travel(350); // travel 35 cm
 		}
-		myRobot.rotate(90); // Turn Left
-		while (!myRobot.isBumperPressed()) {
-			myRobot.travel(350); // travel 35 cm
-			if (myRobot.getCSenseColor().equals(Color.GREEN)) {
-				System.out.println("found Green cell at location (" + myRobot.getX() + "," + myRobot.getY()
-						+ ") with heading " + myRobot.getHeading());
+		robotMonitors[1].rotate(90); // Turn Left
+		while (!robotMonitors[1].isBumperPressed()) {
+			robotMonitors[1].travel(350); // travel 35 cm
+			if (robotMonitors[1].getCSenseColor().equals(Color.GREEN)) {
+				System.out.println("found Green cell at location (" + robotMonitors[1].getX() + ","
+						+ robotMonitors[1].getY() + ") with heading " + robotMonitors[1].getHeading());
 			}
 		}
 
